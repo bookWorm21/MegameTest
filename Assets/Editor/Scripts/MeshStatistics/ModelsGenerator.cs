d26c60697d2c130e183828562f34002f94d421da
bookWorm <golosov2215>
2021-05-30 10:46:40 +0300

    add models object generator

---
+++ Assets/Editor/Scripts/MeshStatistics/ModelsGenerator.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEditor;
+using System.Linq;
+
+namespace Assets.Editor.Scripts.MeshStatistics
+{
+    public static class ModelsGenerator
+    {
+        public static Model[] GenerateModels(MeshFilter[] meshFilters)
+        {
+            var groupingModels = meshFilters.GroupBy(f => f.sharedMesh).
+                Select(g => new { Mesh = g.Key, Count = g.Count()});
+
+            Model[] models = new Model[groupingModels.Count()];
+            int i = 0;
+
+            foreach (var group in groupingModels)
+            {
+                Debug.Log($"{group.Mesh} : {group.Count}");
+                Mesh mesh = group.Mesh;
+
+                string path = AssetDatabase.GetAssetPath(mesh);
+                ModelImporter importer = (ModelImporter)AssetImporter.GetAtPath(path);
+
+                models[i] = new Model(group.Mesh.name,
+                                      path,
+                                      mesh.vertexCount,
+                                      mesh.triangles.Length / 3,
+                                      group.Count,
+                                      group.Count * mesh.vertexCount,
+                                      mesh.GetInstanceID(),
+                                      new ModelImportSettings(mesh.isReadable, importer.generateSecondaryUV));
+
+                i++;
+            }
+
+                return models;
+        }
+    }
+}
